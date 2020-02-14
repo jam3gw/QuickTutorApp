4c61fa47ae4fcaea21856726d0235e882d00d4ed
Jake Moses <mosesjake32@gmail.com>
2020-02-14 14:42:12 -0500

    Creation of basic User model from Django User

--- QuickTutor/models.py
+++ QuickTutor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(User):
+    email = User.email
+    username = User.email
+    password = User.password
+    first_name = User.first_name
+    last_name = User.last_name
+
+    def __str__(self):
+        return self.username
