1d4132fdc82ca96c83a77fbc83113bc2d658dec5
Jake Moses <mosesjake32@gmail.com>
2020-02-23 11:46:23 -0500

    models/settings updates

--- QuickTutor/models.py
+++ QuickTutor/models.py
@@ -1,11 +1,7 @@
-from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class QTUser(AbstractUser):
     pass
-
-    def __str__(self):
-        return self.username
 
     
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -46,8 +47,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     #includes google provider as a login
     'allauth.socialaccount.providers.google',
-
-
 
 ]
 
