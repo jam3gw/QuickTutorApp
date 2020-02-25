9f249ec53357c3b0ae2051a7b997538d433f756a
Jake Moses <mosesjake32@gmail.com>
2020-02-24 21:40:11 -0500

    Active Redirect Page with working Hello <user>

--- QuickTutor/views.py
+++ QuickTutor/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from allauth.account.views import LoginView, SignupView, LogoutView, PasswordResetView
+from django.views import generic
+from .models import QTUser
 
 def index(request):
     return render(request, 'QuickTutor/homepage.html', {})
 # Create your views here.
+
+class temporaryRedirectView(generic.TemplateView):
+    template_name = 'QuickTutor/tempredirect.html'
+    context_object_name = 'user'
+    
     
