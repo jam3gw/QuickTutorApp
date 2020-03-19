0c0e101f9dd96f0891a7a5708e1774690b676377
Jake Moses <mosesjake32@gmail.com>
2020-03-19 09:06:52 -0400

    updated Review model for correct date and time of review

--- QuickTutor/models.py
+++ QuickTutor/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from enum import Enum
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 
 class QTUser(AbstractUser):
@@ -42,7 +43,7 @@ class Review(models.Model):
     rating = models.IntegerField(null=False,choices=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')],help_text="Please rate your experience.")
     description = models.TextField(help_text="Please enter some additional information regarding your experience")
     isTutor = models.BooleanField()
-    time_of_review = models.DateTimeField(auto_now_add=True)
+    time_of_review = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
         return str(Author) + "'s review of " + str(Recipient)
