3d268780fbb50f9371181a62a65d7c141c027982
Jake Moses <mosesjake32@gmail.com>
2020-03-18 23:01:11 -0400

    changed models and admin for many to many relationships instead of foreign key

--- QuickTutor/admin.py
+++ QuickTutor/admin.py
@@ -3,50 +3,52 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import *
-from .models import QTUser, Review, Class, ClassNeedsHelp, TutorableClass
-
-class classNeedsHelpInLine(admin.TabularInline):
-    model = ClassNeedsHelp
-    extra = 1
-
-class TutorableClassInLine(admin.TabularInline):
-    model = TutorableClass
-    extra = 1
+from .models import QTUser, Review, Class, ClassNeedsHelp, TutorableClass, Session
 
 class QTUserAdmin(admin.ModelAdmin):
     model = QTUser
     list_display = ['username', 'first_name','last_name']
     fields = ['email', 'username', 'first_name', 'last_name', 'year']
-    inlines = (classNeedsHelpInLine, TutorableClassInLine)
 
 class ClassAdmin(admin.ModelAdmin):
     model = Class
     list_display = ['class_name', 'dept', 'course_num']
     fields = ['class_name', 'dept', 'course_num']
-    inlines = (classNeedsHelpInLine,TutorableClassInLine)
+
+class SessionAdmin(admin.ModelAdmin):
+    model = Session
+    # list_display = ['Student', 'Tutor']
+    fields = ['date_and_time', 'length']
+    filter_horizontal = ['Student', 'Tutor', 'subject_in_regards_to']
+
 
 class ReviewAdmin(admin.ModelAdmin):
     model = Review
-    # list_display = ('author', 'recipient')
-    # fields = ['author', 'recipient', 'subject_in_regards_to', 'rating', 'description']
+    # list_display = ['Author', 'Recipient']
+    fields = [ 'rating', 'description', 'isTutor', 'time_of_review']
+    filter_horizontal = ['Author', 'Recipient', 'subject_in_regards_to']
 
     # def getAuthor(self, obj):
     #     return "\n".join([p.products for p in obj.product.all()])
 
+
 class ClassNeedsHelpAdmin(admin.ModelAdmin):
     model = ClassNeedsHelp
-    list_diplay = ['user', 'class_id']
-    fields = ['user', 'class_id', 'elaboration']
-
+    # list_diplay = ['user', 'class_id']
+    fields = ['elaboration']
+    filter_horizontal = ['user', 'class_id']
 
 class TutorableClassAdmin(admin.ModelAdmin):
     model = TutorableClass
-    list_diplay = ['user', 'class_id']
-    fields = ['user', 'class_id', 'TA','experience']
+    # list_diplay = ['user', 'class_id']
+    fields = ['TA','experience_detail']
+    filter_horizontal = ['user', 'class_id']
+
 
 admin.site.register(QTUser, QTUserAdmin)
 admin.site.register(Class, ClassAdmin)
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(Session, SessionAdmin)
 admin.site.register(ClassNeedsHelp, ClassNeedsHelpAdmin)
 admin.site.register(TutorableClass, TutorableClassAdmin)
 
--- QuickTutor/models.py
+++ QuickTutor/models.py
@@ -15,30 +15,54 @@ class QTUser(AbstractUser):
 
 
 class Class(models.Model):
-    class_name = models.CharField(max_length=50, null=False)
-    dept = models.CharField(max_length = 6, default="XXXX", null=False)
-    course_num = models.IntegerField(default="0000", null=False)
+    class_name = models.CharField(max_length=50)
+    dept = models.CharField(max_length = 6, default="XXXX")
+    course_num = models.IntegerField(default="0000")
 
     def __str__(self):
         return str(self.dept) + str(self.course_num) + " (" + str(self.class_name) + ")"
 
+class Session(models.Model):
+    Student = models.ManyToManyField(QTUser, related_name="Student", default = 1)
+    Tutor = models.ManyToManyField(QTUser, related_name="Tutor", default = 1) 
+    subject_in_regards_to = models.ManyToManyField(Class, default = 1)
+    date_and_time = models.DateTimeField(null = False)
+    length = models.DurationField(null = False, default = 1)
+
+    def __str__(self):
+        return str(Student) + " is having a session with " + str(Tutor) + " in " + str(subject_in_regards_to) + " " + str(date_and_time) + " for " + str(length)
+
+
+TUTOR_OR_STUDENT=[('S','Student'),('T','Tutor')]
+
 class Review(models.Model):
-    Author = models.ManyToManyField(QTUser, related_name="Author")
-    Recipient = models.ManyToManyField(QTUser,related_name="Recipient")
-    subject_in_regards_to = models.CharField(max_length=30, null=False)
+    Author = models.ManyToManyField(QTUser, related_name = "Author", default = 1)
+    Recipient = models.ManyToManyField(QTUser,related_name="Recipient", default = 1)
+    subject_in_regards_to = models.ManyToManyField(Class, default = 1)
     rating = models.IntegerField(null=False,choices=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')],help_text="Please rate your experience.")
     description = models.TextField(help_text="Please enter some additional information regarding your experience")
+    isTutor = models.BooleanField()
+    time_of_review = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(Author) + "'s review of " + str(Recipient)
 
     
 class ClassNeedsHelp(models.Model):
-    user = models.ForeignKey(QTUser, on_delete=models.CASCADE, default = 1)
-    class_id = models.ForeignKey(Class, on_delete= models.CASCADE, default = 1)
+    user = models.ManyToManyField(QTUser, default = 1)
+    class_id = models.ManyToManyField(Class,  default = 1)
     elaboration = models.TextField(max_length = None, primary_key= False)
+
+    def __str__(self):
+        return str(user) + " needs help in " + str(class_id)
         
 
 class TutorableClass(models.Model):
-    user = models.ForeignKey(QTUser, on_delete=models.CASCADE, default = 1)
-    class_id = models.ForeignKey(Class, on_delete= models.CASCADE, default = 1)
+    user = models.ManyToManyField(QTUser, default = 1)
+    class_id = models.ManyToManyField(Class, default = 1)
     TA = models.BooleanField(name="TA", default=False)
     experience_detail = models.TextField(name="experience", max_length=None)
+
+    def __str__(self):
+        return str(user) + " can tutor in " + str(class_id)
 
