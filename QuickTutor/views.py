689c1da72c6437c1e6e0e9c1a22ff20be164619d
Jake Moses <mosesjake32@gmail.com>
2020-03-29 22:40:34 -0400

    added all context objects

--- QuickTutor/views.py
+++ QuickTutor/views.py
@@ -16,6 +16,26 @@ class ProfileView(generic.TemplateView):
 
     def get(self,request):
         user = request.user
-        tutored_sessions = 
-        led_sessions = 
-        return render(request, self.template_name)
+
+        #sessions participated in 
+        student_sessions = Session.objects.filter(student = user)
+        tutor_sessions = Session.objects.filter(tutor = user)
+
+        #classes need help in/can tutor in 
+        classes_need_help_in = ClassNeedsHelp.objects.filter(user = user)
+        classes_can_tutor_in = TutorableClass.objects.filter(user = user)
+
+        #reviews
+        reviews_received = Review.objects.filter(Recipient = user)
+        reviews_written = Review.objects.filter(Author = user)
+
+        context_objects = {
+            'user' : user,
+            'student_sessions': student_sessions,
+            'tutor_sessions' : tutor_sessions,
+            'classes_need_help_in' : classes_need_help_in,
+            'classes_can_tutor_in' : classes_can_tutor_in,
+            'reviews_received' : reviews_received,
+            'reviews_written' : reviews_written
+        }
+        return render(request, self.template_name, context = context_objects)
