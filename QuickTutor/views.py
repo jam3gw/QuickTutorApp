6a6d9c54c2792451edf87cd56d9a9c78b3b0e473
Jake Moses <mosesjake32@gmail.com>
2020-03-30 10:03:42 -0400

    All data is viewable in the profile.html page

--- QuickTutor/views.py
+++ QuickTutor/views.py
@@ -15,16 +15,37 @@ class ProfileView(generic.TemplateView):
         user = request.user
 
         #sessions participated in 
-        student_sessions = Session.objects.filter(student = user)
-        tutor_sessions = Session.objects.filter(tutor = user)
+        if (len(Session.objects.filter(student = user)) != 0):
+            student_sessions = Session.objects.filter(student = user).get()
+        else : 
+            student_sessions = 'None'
+
+        if (len(Session.objects.filter(tutor = user)) != 0):
+            tutor_sessions = Session.objects.filter(tutor = user).get()
+        else : 
+            tutor_sessions = 'None'
 
         #classes need help in/can tutor in 
-        classes_need_help_in = ClassNeedsHelp.objects.filter(user = user)
-        classes_can_tutor_in = TutorableClass.objects.filter(user = user)
+        if (len(ClassNeedsHelp.objects.filter(user = user)) != 0 ):
+            classes_need_help_in = ClassNeedsHelp.objects.filter(user = user).get()
+        else: 
+            classes_need_help_in = 'None'
+
+        if (len(TutorableClass.objects.filter(user = user)) != 0):
+            classes_can_tutor_in = TutorableClass.objects.filter(user = user).get()
+        else:
+            classes_can_tutor_in = 'None'
 
         #reviews
-        reviews_received = Review.objects.filter(Recipient = user)
-        reviews_written = Review.objects.filter(Author = user)
+        if (len(Review.objects.filter(Recipient = user)) != 0):
+            reviews_received = Review.objects.filter(Recipient = user).get()
+        else:
+            reviews_received = 'None'
+
+        if (len(Review.objects.filter(Author = user)) != 0):
+            reviews_written = Review.objects.filter(Author = user).get()
+        else:
+            reviews_written = 'None'
 
         context_objects = {
             'user' : user,
