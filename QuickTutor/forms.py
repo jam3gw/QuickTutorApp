606111e05e5ebf66632aa3fa897579c3e7ba98ac
Jake Moses <mosesjake32@gmail.com>
2020-04-06 11:15:41 -0400

    patched dat boi

--- QuickTutor/forms.py
+++ QuickTutor/forms.py
@@ -20,7 +20,7 @@ class SessionForm(forms.ModelForm):
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['Recipient','subject_in_regards_to','description','type_of_review','time_of_review']
+        fields = ['Recipient','subject_in_regards_to','rating','description','type_of_review','time_of_review']
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
