38c80921f40fcbff3a854b52645b7b6f0e57ab78
Jake Moses <mosesjake32@gmail.com>
2020-04-12 10:37:22 -0400

    no longer need success.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,8 +35,6 @@ urlpatterns = [
     path('profile/new-session/', Create_Session),
     path('profile/new-session/<tutor_id>', createSessionSpecific, name = "specific_session_creation"),
     path('profile/edit-profile/', edit_Profile_Class),
-
-    path('sessions/success/', TemplateView.as_view(template_name="QuickTutor/success.html")),
     path('profile/delete_session/<session_id>/', deleteSession, name= "delete_session"),
     path('profile/delete_class_needs_help/<class_needs_help_id>/', deleteClassNeedsHelp, name= "delete_class_needs_help"),
     path('profile/delete_tutorable_class/<tutorable_class_id>/', deleteTutorableClass, name= "delete_tutorable_class"),
